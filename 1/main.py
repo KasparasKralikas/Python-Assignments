657768e4af545e045616c46488266ae7f3e18e20
KasparasKralikas <kasparaskr@unity3d.com>
2020-04-22 18:37:51 +0300

    Implemented 11th and 12th parts of first assignment

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,6 @@
 import numpy
+import scipy.misc
+import scipy.integrate
 
 #1
 def splitInterval(start, end, n):
@@ -68,15 +70,60 @@ def inverseMatrix(matrix):
 def getEigenValuesAndVector(matrix):
     return numpy.linalg.eig(matrix)
 
+#11
+def calculateDerivative(f, x0, dx):
+    return scipy.misc.derivative(f, x0, dx)
+
+#12
+def calculateDefinitiveIntegral(f, x0, x1):
+    return scipy.integrate.quad(f, x0, x1)
+
+#12*
+def calculateIndefinitiveIntegral(polynomial):
+    return numpy.polyint(polynomial)
+
+
+def f(x):
+    return x**2 + 1
+
 def main():
+    print("1: ")
     print(numpy.arange(start=-1.3, stop=2.5, step=((2.5 - (-1.3)) / 64)))
+
+    print("2: ")
     print(generate123Array(3))
+
+    print("3: ")
     print(generateArrayOfOddNumbers(10))
+
+    print("4: ")
     print(generateFrammedArray(10))
+
+    print("5: ")
     print(generateChessBoard(8))
+
+    print("6: ")
     print(generateIPlusJArray(10))
+
+    print("7: ")
     print(getArraySums(numpy.random.rand(3, 5)))
+
+    print("8: ")
     print(sortByCol(numpy.random.rand(5, 5), 1))
+
+    print("9: ")
     print(inverseMatrix(numpy.random.rand(5, 5)))
+
+    print("10: ")
     print(getEigenValuesAndVector(numpy.random.rand(5, 5)))
+
+    print("11: ")
+    print(calculateDerivative(f, 1, 1e-6))
+
+    print("12: ")
+    print(calculateDefinitiveIntegral(f, 0, 1))
+
+    print("12*: ")
+    print(calculateIndefinitiveIntegral([1, 1]))
+
 main()

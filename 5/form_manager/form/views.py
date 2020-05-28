3b8723760870c689c0207350e411dfeb252f9e1a
KasparasKralikas <kasparaskr@unity3d.com>
2020-05-28 08:59:30 +0300

    added mock data

--- 5/form_manager/form/views.py
+++ 5/form_manager/form/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+filled_forms = [
+    {
+        'from': 'Joe',
+        'to': 'Josh',
+        'date': '2020-05-20',
+        'subject': 'purchase'
+
+    },
+    {
+        'from': 'Joe',
+        'to': 'Will',
+        'date': '2020-05-21',
+        'subject': 'purchase 1'
+
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'filled_forms': filled_forms
+    }
+    return render(request, 'form/home.html', context)
 
 def form(request):
-    return HttpResponse('<h1>Form</h1>')
+    return render(request, 'form/form.html')
